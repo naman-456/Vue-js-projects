82141d1c70ea36869a7d4f193a6509b3c2f328f5
Naman Mamodia <60863743+naman-456@users.noreply.github.com>
2023-05-27 13:16:57 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,40 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView
+  },
+  {
+    path: '/about',
+    name: 'about',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  },
+  {
+     path: "/newpage",
+     name: "newpage",
+     component : () => import("../views/newPage.vue")
+  },
+  {
+    path:"/css-practice",
+    name:"CssPractice",
+    component: () => import("../views/CssPractice.vue")
+  },
+  {
+    path:"/myform",
+    name:"MyForm",
+    component:() => import("../views/MyForm.vue")
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+
+export default router
